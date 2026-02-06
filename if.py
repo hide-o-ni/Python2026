70604623ddf9b27c78fa3436e3a54b744749fbd3
Ольга <65725996+hide-o-ni@users.noreply.github.com>
2026-02-07 00:10:43 +0300

    Add files via upload

---
+++ if.py
@@ -0,0 +1,16 @@
+# Чтение входных данных
+score = int(input())
+
+if score>=90 and score<=100:
+    grade = "A"
+elif score>=80 and score<=89:
+    grade ="B"
+elif score>=70 and score<=79:
+    grade="C"
+elif score>=60 and score<=69:
+    grade ="D"
+else:
+    grade ="F"
+
+# Вывод результата
+print(grade)
